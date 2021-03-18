84eca8cb49f73de9a28e60007509c2be377a7c0f
mk10969 <mk10969@gmail.com>
2021-03-19 01:46:13 +0900

    argocd-notification app 作成

--- argocd-notification/update.sh
+++ argocd-notification/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -sfL https://raw.githubusercontent.com/argoproj-labs/argocd-notifications/stable/manifests/install.yaml -o ./base/upstream/install.yaml
-
 # images/0/newTagは、arm64に対応してあるlatestを指定。
+curl -sfL https://raw.githubusercontent.com/argoproj-labs/argocd-notifications/release-1.0/manifests/install.yaml -o ./base/upstream/01-install.yaml
+curl -sfL https://raw.githubusercontent.com/argoproj-labs/argocd-notifications/release-1.0/catalog/install.yaml -o ./base/upstream/02-configmap.yaml
