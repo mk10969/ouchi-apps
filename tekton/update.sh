6cbcb1152420fed6fef1c281bbbbbdcd5f255495
mk10969 <mk10969@gmail.com>
2021-03-19 19:59:38 +0900

    mv

--- tekton/update.sh
+++ tekton/update.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-curl -L https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml -o 01-pipeline.yaml
-curl -L https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml -o 02-triggers.yaml
-curl -L https://github.com/tektoncd/dashboard/releases/latest/download/tekton-dashboard-release.yaml -o 03-dashboard.yaml
+PIPLINE_VERSION="v0.22.0"
+TRIGGER_VERSION="v0.12.1"
+DASHBOARD_VERSION="v0.15.0"
+
+curl -sfL https://storage.googleapis.com/tekton-releases/pipeline/previous/$PIPLINE_VERSION/release.yaml -o ./base/upstream/01-pipeline.yaml
+curl -sfL https://storage.googleapis.com/tekton-releases/triggers/previous/$TRIGGER_VERSION/release.yaml -o ./base/upstream/02-triggers.yaml
+curl -sfL https://github.com/tektoncd/dashboard/releases/download/$DASHBOARD_VERSION/tekton-dashboard-release.yaml -o ./base/upstream/03-dashboard.yaml
