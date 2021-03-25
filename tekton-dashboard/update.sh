77c20ebffe50176161b96853a02510c184b5b69a
mk10969 <mk10969@gmail.com>
2021-03-25 23:14:20 +0900

    dashboradをreadonlyに変更

--- tekton-dashboard/update.sh
+++ tekton-dashboard/update.sh
@@ -3,3 +3,4 @@
 DASHBOARD_VERSION="v0.15.0"
 
 curl -sfL https://github.com/tektoncd/dashboard/releases/download/$DASHBOARD_VERSION/tekton-dashboard-release.yaml -o ./base/upstream/tekton-dashboard.yaml
+curl -sfL https://github.com/tektoncd/dashboard/releases/download/$DASHBOARD_VERSION/tekton-dashboard-release-readonly.yaml -o ./base/upstream/tekton-dashboard-readonly.yaml
