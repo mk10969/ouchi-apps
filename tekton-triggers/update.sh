80cc750a5c07fd02be51d6dd4affd475f635547e
mk10969 <mk10969@gmail.com>
2021-04-23 03:14:26 +0900

    tekton-trigger update

--- tekton-triggers/update.sh
+++ tekton-triggers/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-TRIGGERS_VERSION="v0.12.1"
+TRIGGERS_VERSION="v0.13.0"
 
 curl -sfL https://storage.googleapis.com/tekton-releases/triggers/previous/$TRIGGERS_VERSION/release.yaml -o ./base/upstream/tekton-triggers.yaml
